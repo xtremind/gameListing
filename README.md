a317a62e08e0ad80038a24e18e6b8c8b06f66f54
Vagrant Default User <vagrant@stretch.localdomain>
2019-12-17 09:25:51 +0000

    fix application name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GameListing2
+# GameListing
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.22.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gameListing2'`, () => {
+  it(`should have as title 'gameListing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gameListing2');
+    expect(app.title).toEqual('gameListing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gameListing2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('gameListing app is running!');
   });
 });
