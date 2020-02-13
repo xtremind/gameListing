4c558fb121c73f612d1f70f1d96ccc4f6828bd11
Guillaume Da Graça <guillaume.da-graca@capgemini.com>
2020-02-13 17:24:56 +0100

    fix tslint error

--- front/src/app/core/services/console/console.service.ts
+++ front/src/app/core/services/console/console.service.ts
@@ -5,7 +5,6 @@ import { Console } from '../../../shared/models/console.model';
 
 import sample from '../../../../assets/data/sample.json';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -15,6 +14,10 @@ export class ConsoleService {
 
   findAll(): Observable<Console[]> {
     // console.log(sample);
-    return of(sample.consoles.map(console => <Console>{name: console.name, version: console.version}));
+    return of(sample.consoles.map((console: Console) => ({name: console.name, version: console.version}) as Console));
+  }
+
+  find(name: string): Observable<Console> {
+    return of(sample.consoles.find((console: Console) => console.name === name));
   }
 }
--- front/src/app/core/services/game/game.service.ts
+++ front/src/app/core/services/game/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+import { Console } from '../../../shared/models/console.model';
 import { Game } from '../../../shared/models/game.model';
 
 import sample from '../../../../assets/data/sample.json';
@@ -14,10 +15,10 @@ export class GameService {
 
   findAll(): Observable<Game[]> {
     // console.log(sample);
-    return of(sample.consoles.map(console => console.games).flat());
+    return of(sample.consoles.map((console: Console) => console.games).flat());
   }
 
   find(id: number): Observable<Game> {
-    return of(sample.consoles.map(console => console.games).flat().find(game => Number(game.id) === id));
+    return of(sample.consoles.map((console: Console) => console.games).flat().find((game: Game) => Number(game.id) === id));
   }
 }
