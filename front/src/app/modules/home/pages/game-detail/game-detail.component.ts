7c89f2af92b0523ac655deb03ba092a3bb11d1af
Guillaume Da Graça <guillaume.da-graca@capgemini.com>
2020-03-12 11:11:55 +0100

    adding game detail frame

--- front/src/app/modules/home/pages/game-detail/game-detail.component.ts
+++ front/src/app/modules/home/pages/game-detail/game-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
 
 import { GameService } from '../../../../core/services/game/game.service';
 
@@ -16,7 +18,15 @@ export class GameDetailComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private gameService: GameService) { }
+    private gameService: GameService,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer) {
+      // FIXME : doesn't display icon
+      iconRegistry.addSvgIcon(
+        'expand_more', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/expand_more-24px.svg'));
+      iconRegistry.addSvgIcon(
+        'expand_less', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/expand_less-24px.svg'));
+    }
 
   ngOnInit(): void {
     this.getDetail();
@@ -26,5 +36,7 @@ export class GameDetailComponent implements OnInit {
     const id: number = Number(this.route.snapshot.paramMap.get('id'));
     this.gameService.find(id).subscribe(game => this.game = game);
   }
+
+  // TODO : add event listener for click on "lire plus" button
 
 }
